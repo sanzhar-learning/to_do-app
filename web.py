e8a4c2d2373c638d7aa5f15e4d4265d9ec81c451
Your Name <sanzhar.learn@gmail.comccd>
2026-01-11 13:41:15 +0100

    transform into web app

---
+++ web.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+st.title("Welcome to my To-Do List App!")
+st.subheader("Your personal task manager")
+st.write("This app allows you to manage your to-do items easily and stay organized.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="", placeholder="Enter a new todo...", on_change=add_todo, key="new_todo")
